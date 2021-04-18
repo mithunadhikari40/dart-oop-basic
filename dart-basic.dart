f1be8f3e9d871585c5cecc7c22cfdae56afa6cb8
Mithun Adhikari <mithunadhikari40@gmail.com>
2021-04-18 07:53:33 +0545

    added a new class Cat and added some interaction between Dog and Cat

--- dart-basic.dart
+++ dart-basic.dart
@@ -1,3 +1,22 @@
+void main () {
+  var dog = new Dog("Oscar","Vote","Male",2,false);
+//   dog.bark();
+//   dog.eat(300);
+//   int age = dog.getAge();
+//   print(age);
+//    bool favoriteDog = dog.isFavoritePet();
+//   print(favoriteDog);
+  
+  var cat = new Cat("Pinky","Local","Female",2,false);
+//   cat.meow();
+//   cat.eat(350);
+//   bool favoriteCat = cat.isFavoritePet();
+  
+  bool hasFavoritePet = dog.isFavoritePet() && cat.isFavoritePet();
+  print(hasFavoritePet);
+  
+}
+
 class Dog {
   String name;
   String breed;
@@ -7,6 +26,57 @@ class Dog {
   
   Dog(this.name, this.breed, this.gender, this.age, this.isFavorite);
   
+  void bark() {
+    print("Dog is barking");
+  }
+  
+  void eat (int foodWeight) {
+    if (foodWeight > 400) {
+      print("Dog is eating the food");
+    } else {
+      bark();
+    } 
+  }
+  
+  int getAge () {
+    return age;
+  }
+  
+  bool isFavoritePet () {
+    return isFavorite;
+  }
+  
+}
+
+
+class Cat {
+  String name;
+  String breed;
+  String gender;
+  int age;
+  bool isFavorite;
+  
+  Cat(this.name, this.breed, this.gender, this.age, this.isFavorite);
+  
+  void meow() {
+    print("Cat is meowing");
+  }
+  
+  void eat (int foodWeight) {
+    if (foodWeight > 300) {
+      print("Cat is eating the food");
+    } else {
+      meow();
+    } 
+  }
+  
+  int getAge () {
+    return age;
+  }
+  
+  bool isFavoritePet () {
+    return isFavorite;
+  }
   
 }
 
