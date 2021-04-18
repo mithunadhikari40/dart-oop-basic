36d17d1ba0da9fa450b78c5fb8eb79fca6f412cd
Mithun Adhikari <mithunadhikari40@gmail.com>
2021-04-18 07:06:12 +0545

    added a new class named Dog

---
+++ dart-basic.dart
@@ -0,0 +1,12 @@
+class Dog {
+  String name;
+  String breed;
+  String gender;
+  int age;
+  bool isFavorite;
+  
+  Dog(this.name, this.breed, this.gender, this.age, this.isFavorite);
+  
+  
+}
+
